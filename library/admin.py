9cb7b434af705e32e3277ce0025b143dbb93d02b
Cristian Rojas <work@cristianrojas.com>
2019-01-08 15:25:14 -0500

    Created all library models

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import Author, Genre, Language, Book, BookInstance
+
+
+@admin.register(Author)
+class PatternAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
--- library/models.py
+++ library/models.py
@@ -1,3 +1,112 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = u'Author'
+        verbose_name_plural = u'Author'
+
+    def __str__(self):
+        return u'Author {name}'.format(
+            name=self.name
+        )
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=50
+    )
+
+    class Meta:
+        verbose_name = u'Genre'
+        verbose_name_plural = u'Genres'
+
+    def __str__(self):
+        return u'Genre {name}'.format(
+            name=self.name
+        )
+
+
+class Language(models.Model):
+    name = models.CharField(
+        max_length=50
+    )
+
+    class Meta:
+        verbose_name = u'Language'
+        verbose_name_plural = u'Languages'
+
+    def __str__(self):
+        return u'Language {name}'.format(
+            name=self.name
+        )
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=255
+    )
+    author = models.ForeignKey(
+        Author,
+        related_name="books",
+        on_delete=models.PROTECT
+    )
+    summary = models.TextField()
+    imprint = models.TextField()
+    ISBN = models.CharField(
+        max_length=15
+    )
+    genres = models.ManyToManyField(
+        Genre,
+        related_name="books"
+    )
+    language = models.ForeignKey(
+        Language,
+        related_name="books",
+        on_delete=models.PROTECT
+    )
+
+    class Meta:
+        verbose_name = u'Book'
+        verbose_name_plural = u'Books'
+
+    def __str__(self):
+        return u'Book {title}'.format(
+            title=self.title
+        )
+
+
+class BookInstance(models.Model):
+    book = models.ForeignKey(
+        Book,
+        related_name="book_instances",
+        on_delete=models.CASCADE
+    )
+    unique_id = models.CharField(
+        max_length=20,
+        unique=True
+    )
+    due_back = models.DateField()
+    status = models.CharField(
+        max_length=10
+    )
+
+    class Meta:
+        verbose_name = u'BookInstance'
+        verbose_name_plural = u'BookInstances'
+
+    def __str__(self):
+        return u'Copy of the book {book_title} with id: {unique_id}'.format(
+            book_title=self.book.title,
+            unique_id=self.unique_id
+        )
+
